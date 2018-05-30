06136339ec4c5bb68d20d2787012a1e2cd75d40c
paulonova <paulonova144@gmail.com>
2018-05-30 08:46:06 +0200

    PHP with MySQl trainning 1

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>globe Bank</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" media="all" href="../stylesheets/staff.css">
+  </head>
+
+  <body>
+
+    <h1>Globe Bank: Coming soon</h1>
+
+  </body>
+</html>
---
+++ public/staff/subjects/show.php
@@ -0,0 +1,13 @@
+<?php
+    require_once('../../../private/initialize.php');
+
+
+    $id = isset($_GET['id']) ? $_GET['id'] : '1'; //ternary < php 7.0
+    //$id = $_GET['id'] ?? '1'; //ternary > php 7.0
+    echo $id;
+
+?>
+
+<a href="show.php?name=<?php echo u('John Doe'); ?>">Link</a><br />
+<a href="show.php?company=<?php echo u('Widgets&More'); ?>">Link</a><br />
+<a href="show.php?query=<?php echo u('!#*?'); ?>">Link</a><br />
