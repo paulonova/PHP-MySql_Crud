a570d5ffd89fa8d296cef34f9e394159816d9e2f
paulonova <paulonova144@gmail.com>
2018-05-31 19:29:19 +0200

    Create and Edit

--- public/staff/subjects/new.php
+++ public/staff/subjects/new.php
@@ -1,18 +1,54 @@
 <?php require_once('../../../private/initialize.php');
 
-    $test = $_GET['test'] ?? '';
+$test = $_GET['test'] ?? '';
 
-    if($test == '404'){
-        error_404();
+if ($test == '404') {
+    error_404();
 
-    }elseif($test == '505'){
-        error_505();
-    }elseif($test == 'redirect'){
-        redirect_to(url_for('/staff/subjects/index.php'));
-    }else{
-        echo 'No Error';
-    }
+} elseif ($test == '505') {
+    error_505();
+} elseif ($test == 'redirect') {
+    redirect_to(url_for('/staff/subjects/index.php'));
+}
 ?>
-
-
 <!-- there is no white spaces between php -->
+
+<?php $page_title = 'Create Subject'; ?>
+<?php include(SHARED_PATH . '/staff_header.php'); ?>
+
+<div id="content">
+
+  <a class="back-link" href="<?php echo url_for('/staff/subjects/index.php'); ?>">&laquo; Back to List</a>
+
+  <div class="subject new">
+    <h1>Create Subject</h1>
+
+    <form action="<?php echo url_for('/staff/subjects/create.php') ?>" method="post">
+      <dl>
+        <dt>Menu Name</dt>
+        <dd><input type="text" name="menu_name" value="" /></dd>
+      </dl>
+      <dl>
+        <dt>Position</dt>
+        <dd>
+          <select name="position">
+            <option value="1">1</option>
+          </select>
+        </dd>
+      </dl>
+      <dl>
+        <dt>Visible</dt>
+        <dd>
+          <input type="hidden" name="visible" value="0" />
+          <input type="checkbox" name="visible" value="1" />
+        </dd>
+      </dl>
+      <div id="operations">
+        <input type="submit" value="Create Subject" />
+      </div>
+    </form>
+
+  </div>
+
+</div>
+
