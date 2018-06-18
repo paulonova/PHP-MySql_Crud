74efc83e890a8790c7653990f62914c34315b6f0
paulonova <paulonova144@gmail.com>
2018-06-18 10:39:33 +0200

    Allow HTML in dynamic content

--- private/shared/public_header.php
+++ private/shared/public_header.php
@@ -1,8 +1,9 @@
 <!doctype html>
 
 <html lang="en">
-  <head>
-    <title>Globe Bank <?php if(isset($page_title)) { echo '- ' . h($page_title); } ?></title>
+<head>
+    <title>Globe Bank <?php if(isset($page_title)) { echo '- ' . h($page_title); } ?>
+    <?php if(isset($preview) && $preview) { echo ' [PREVIEW]'; } ?></title>
     <meta charset="utf-8">
     <link rel="stylesheet" media="all" href="<?php echo url_for('/stylesheets/public.css'); ?>" />
   </head>
